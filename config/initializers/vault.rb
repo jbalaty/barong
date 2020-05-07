26eede46226e6d4656df8b8b2fd25b32798c448a
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-05-07 19:15:15 +0300

    Add secret encrypted to api keys model

--- config/initializers/vault.rb
+++ config/initializers/vault.rb
@@ -3,6 +3,7 @@
 require 'vault/rails'
 
 Vault.configure do |config|
+  config.enabled = Rails.env.production?
   config.address = Barong::App.config.vault_address
   config.token = Barong::App.config.vault_token
   config.ssl_verify = false
--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :api_key, class: 'APIKey' do
     user
     kid { Faker::Crypto.sha256 }
+    secret { SecureRandom.hex(16) }
     scope { %w[trade] }
     algorithm { 'HS256' }
   end
